6fc4d921258058122cb2f7ea59b6894e693b832d
Haria.Informatica <haria.informatica@gmail.com>
2024-05-21 11:41:19 +0100

    textual button example

--- textual1/button.py
+++ textual1/button.py
@@ -1,26 +1,30 @@
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, VerticalScroll
-from textual.widgets import Button, Static
+from textual.binding import Binding
+from textual.widgets import Button, Static, Footer
 
 
 class ButtonsApp(App[str]):
+    BINDINGS = [
+        Binding(key="q", action="quit", description="Salir")
+    ]
     CSS_PATH = "button.tcss"
-
-    button1 = Button("Saludar")
-    button2 = Button.error("Salir")
 
     def compose(self) -> ComposeResult:
         
+        yield Footer(),
         yield Horizontal(
             VerticalScroll(
                 Static("Saludar al mundo", classes="header"),
-                self.button1,
-                self.button2
+                Button("Saludar", id="butSaludar"),
+                Button.error("Salir", id="butSalir")
             ),
         )
+        
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
-        if Button.label == "Salir":
+
+        if event.button.id == "butSalir":
             self.exit(str(event.button))
         else:
             self.notify(
