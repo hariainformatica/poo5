e82e48415aa935501c93f1813b37918352444359
Haria.Informatica <haria.informatica@gmail.com>
2024-05-29 13:07:01 +0100

    dos ventanas con widgets

--- textual2/modes01.py
+++ textual2/modes01.py
@@ -1,19 +1,37 @@
 from textual.app import App, ComposeResult
 from textual.screen import Screen
-from textual.widgets import Placeholder, Button
+from textual.widgets import Placeholder, Button, DataTable, Input
+from textual.containers import Horizontal
 
+ROWS = [
+    ("ID", "Nombre"),
+    (1, "Yoel"),
+    (4, "Samuel"),
+    (7, "Amaro"),
+]
 
 class MainScreen(Screen):
     def compose(self) -> ComposeResult:
-        yield Placeholder("Pantalla Principal")
+        yield DataTable()
         yield Button("Edición")
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         self.app.switch_to_edit()
 
+    def on_mount(self) -> None:
+        table = self.query_one(DataTable)
+        table.cursor_type = "row"
+        table.zebra_stripes = True
+        table.add_columns(*ROWS[0])
+        table.add_rows(ROWS[1:])
+
 class EditScreen(Screen):
     def compose(self) -> ComposeResult:
-        yield Placeholder("Pantalla Edición")
+        yield Input(placeholder="Nombre")
+        yield Horizontal(
+                Button("Aceptar"),
+                Button("Cancelar"),
+        )
         yield Button("Principal")
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
@@ -30,7 +48,7 @@ class ModesApp(App):
         "edit": EditScreen,
     }
 
-    def on_mount(self) -> None:
+    def on_mount(self) -> None:    
         self.switch_mode("main")
 
     def switch_to_edit(self) -> None:
