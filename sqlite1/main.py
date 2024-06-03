189e577ec6dd3d5f3ce75d904f433301abd987e6
Haria.Informatica <haria.informatica@gmail.com>
2024-06-03 13:07:42 +0100

    sqlite3

---
+++ sqlite1/main.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+SQLMDLCREATE = '''
+    CREATE TABLE IF NOT EXISTS citas (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+   	cita TEXT NOT NULL
+)
+'''
+
+#con = sqlite3.connect('citas.db', isolation_level = None)
+con = sqlite3.connect('citas.db', autocommit=True)
+
+con.execute(SQLMDLCREATE)
+
+SQLDDLINSERT = '''
+    INSERT INTO citas (cita) VALUES ('La vida es bella')
+'''
+
+con.execute(SQLDDLINSERT)
+#con.commit()
+
+SQLDDLSELECT = '''
+    SELECT * FROM citas
+'''
+
+res = con.execute(SQLDDLSELECT)
+print(res.fetchall())
+
+con.close()
